29e3e617b007d6966ccc4c74aac96fdaa364efd2
James Shakespeare <james@bighuman.com>
2014-01-09 11:38:10 -0500

    page 17

--- assets/coffee/app.coffee
+++ assets/coffee/app.coffee
@@ -1,1 +1,36 @@
-app.coffee
+ContactManager = new Marionette.Application()
+
+# Declare our regions
+
+ContactManager.addRegions
+  mainRegion: "#main-region"
+
+# Declare our models
+
+ContactManager.Contact = Backbone.Model.extend({})
+
+# Declare our views
+
+ContactManager.StaticView = Marionette.ItemView.extend
+  template: "#static-template"
+
+ContactManager.ContactView = Marionette.ItemView.extend
+  template: "#contact-template"
+
+# Init
+
+ContactManager.on "initialize:after", ->
+  
+  # Create a new contact and a view
+
+  alice = new ContactManager.Contact
+    firstName: "Alice"
+    lastName: "Arten"
+    phoneNumber: "555-0184"
+
+  aliceView = new ContactManager.ContactView
+    model: alice
+
+  ContactManager.mainRegion.show aliceView
+
+ContactManager.start()
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,35 @@
-app.js
+(function() {
+  var ContactManager;
+
+  ContactManager = new Marionette.Application();
+
+  ContactManager.addRegions({
+    mainRegion: "#main-region"
+  });
+
+  ContactManager.Contact = Backbone.Model.extend({});
+
+  ContactManager.StaticView = Marionette.ItemView.extend({
+    template: "#static-template"
+  });
+
+  ContactManager.ContactView = Marionette.ItemView.extend({
+    template: "#contact-template"
+  });
+
+  ContactManager.on("initialize:after", function() {
+    var alice, aliceView;
+    alice = new ContactManager.Contact({
+      firstName: "Alice",
+      lastName: "Arten",
+      phoneNumber: "555-0184"
+    });
+    aliceView = new ContactManager.ContactView({
+      model: alice
+    });
+    return ContactManager.mainRegion.show(aliceView);
+  });
+
+  ContactManager.start();
+
+}).call(this);
